71ac2cb983e2e915dbd8ba162c40b4998e711f46
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 16:17:14 +0400

    2

--- app.js
+++ app.js
@@ -6,27 +6,17 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-//const PORT = config.get("port") || 5000;
-const PORT = process.env.PORT || 5000;
 
+app.use(express.json({ extended: true }));
 app.use(
     cors({
         origin: ["http://localhost:3000", "https://mern-task-four-client.onrender.com"],
     })
 );
-
-app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use("/", express.static(path.join(__dirname, "client", "build")));
-
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     });
-// }
-
+const PORT = process.env.PORT || 5000;
 async function start() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
