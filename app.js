7382130999daa25fb9c16e4a66b3a7c58e482291
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 19:15:56 +0400

    let's try this

--- app.js
+++ app.js
@@ -13,17 +13,29 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
 const PORT = process.env.PORT || 5000;
-async function start() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
-    } catch (error) {
-        console.log("Server error", error.message);
-        process.exit(1);
-    }
-}
+// function start() {
+//     try {
+//         mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+//     } catch (error) {
+//         console.log("Server error", error.message);
+//         process.exit(1);
+//     }
+// }
 
-start();
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => console.log(err));
+
+//start();
