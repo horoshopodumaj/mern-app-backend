1869d5f52966836def72f1cc91a780254f89895c
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 15:51:22 +0400

    1

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express();
 //const PORT = config.get("port") || 5000;
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://mern-task-four-client.onrender.com"],
+    })
+);
 
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
