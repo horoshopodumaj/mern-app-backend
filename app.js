9f99c23ebb75978f7391a7e0f64bf51dda96683f
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 20:40:09 +0400

    I even understand a little what's going on, but not completely

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json({ extended: true }));
-app.use(cors());
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
