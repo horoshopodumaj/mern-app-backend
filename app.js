2506ad85ed12f82f7d06e8f79899877752c45124
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 18:57:32 +0400

    I'm not losing hope

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json({ extended: true }));
-app.use(
-    cors({
-        origin: ["http://localhost:3000", "https://mern-task-four-client.onrender.com"],
-    })
-);
+app.use(cors());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
