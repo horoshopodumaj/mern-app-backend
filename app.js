299e44272301d834eba2235670333d012ac9797f
Diana Murtazina <murka8831@yandex.ru>
2022-11-27 15:02:11 +0400

    I'll try this

--- app.js
+++ app.js
@@ -17,13 +17,19 @@ app.use(
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
-if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname, "build")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use("/", express.static(path.join(__dirname, "build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "build", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//     });
+// }
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 // function start() {
