7ccee2db5cf66101372b32ee361850b83879e5a7
Diana Murtazina <murka8831@yandex.ru>
2022-11-27 19:33:13 +0400

    maybe now everything will break

--- app.js
+++ app.js
@@ -17,13 +17,11 @@ app.use(
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use("/", express.static(path.join(__dirname, "build")));
+app.use("/", express.static(path.join(__dirname, "client", "build")));
 
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "build", "index.html"));
-//     });
-// }
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // app.use(express.static(path.join(__dirname, "build")));
 
