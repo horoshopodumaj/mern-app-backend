42f11bf84bba5952251748728e459c9305a526ab
Diana Murtazina <murka8831@yandex.ru>
2022-11-27 20:00:23 +0400

    I'm not looking for easy ways, yeah

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use(
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
-app.use("/", express.static(path.join(__dirname, "client", "build")));
+app.use("/", express.static(path.join(__dirname, "client", "public")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 });
 
 // app.use(express.static(path.join(__dirname, "build")));
